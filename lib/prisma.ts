55bc4edca542e6c4fc58ff62385e17a3fa0f2185
saimeunt <saimeunt@gmail.com>
2024-12-21 16:41:54 +0100

    protect api route

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,18 @@
-import { PrismaClient } from '@/prisma/generated/client';
+import { PrismaClient } from '@/prisma/generated/client'
+import { currentUser } from '@clerk/nextjs/server'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export const fetchUser = async () => {
+  const user = await currentUser()
+  if (!user) {
+    return null
+  }
+  return prisma.user.findUnique({
+    where: { userId: user.id }
+  })
+}
